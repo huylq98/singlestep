dae1b71b57d19d65a010d9008dad6108a77fd319
huylq33 <huylq33@viettel.com.vn>
2026-05-13 14:01:22 +0700

    feat(sim-kit): add barrel export

---
+++ src/components/sim-kit/index.ts
@@ -0,0 +1,25 @@
+export { Stage } from './Stage';
+export type { StageProps } from './Stage';
+
+export { Node } from './Node';
+export type { NodeProps, NodeState } from './Node';
+
+export { Arrow } from './Arrow';
+export type { ArrowProps, Point } from './Arrow';
+
+export { Toggle } from './Toggle';
+export type { ToggleProps, ToggleOption } from './Toggle';
+
+export { Slider } from './Slider';
+export type { SliderProps } from './Slider';
+
+export { StepButton } from './StepButton';
+export type { StepButtonProps } from './StepButton';
+
+export { Legend } from './Legend';
+export type { LegendProps, LegendItem } from './Legend';
+
+export { EventLog } from './EventLog';
+export type { EventLogProps, LogEntry, LogKind } from './EventLog';
+
+export { colors, motion, radii } from './tokens';
